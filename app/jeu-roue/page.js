a035095c1f14bbbf08aa9fc1a175b33fb8d37fcd
Web-Online-Concept <web.online.concept@gmail.com>
2025-07-02 15:11:03 +0200

    Update page.js

--- app/jeu-roue/page.js
+++ app/jeu-roue/page.js
@@ -135,7 +135,7 @@ export default function JeuRoue() {
                   <div className="relative">
                     <div className="relative w-80 h-80 mx-auto">
                       {/* Flèche indicatrice */}
-                      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-2 z-10">
+                      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-2 z-10">
                         <div className="w-0 h-0 border-l-[20px] border-l-transparent border-r-[20px] border-r-transparent border-t-[40px] border-t-red-600"></div>
                       </div>
                       
