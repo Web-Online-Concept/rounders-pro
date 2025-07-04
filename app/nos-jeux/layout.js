e2bbe229f44a008c7f736cac6c5b82786937dd6b
Web-Online-Concept <web.online.concept@gmail.com>
2025-07-04 17:07:22 +0200

    seo

---
+++ app/nos-jeux/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Nos Jeux Gratuits - Roue & Concours | Sans obligation - Rounders',
+  description: 'Découvrez tous nos jeux gratuits : Roue de la Fortune, concours Twitter. Participation gratuite, gains offerts à notre discrétion.',
+  keywords: 'jeux gratuits rounders, roue fortune, concours twitter',
+};
+
+export default function NosJeuxLayout({ children }) {
+  return children;
+}
---
+++ app/roue-fortune/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Roue de la Fortune Gratuite - Jusqu\'à 50€ | Sans garantie - Rounders',
+  description: 'Tournez la roue gratuitement une fois par jour. Gains jusqu\'à 50€ offerts gracieusement sans obligation. Jeu marketing à notre discrétion.',
+  keywords: 'roue fortune gratuite, wheel of fortune, jeu quotidien rounders',
+};
+
+export default function RoueFortuneLayout({ children }) {
+  return children;
+}
