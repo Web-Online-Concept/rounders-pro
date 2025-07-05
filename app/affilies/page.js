ebca2033d2537c2358cdcb2f9de93a994c762b33
Web-Online-Concept <web.online.concept@gmail.com>
2025-07-05 03:52:25 +0200

    Update page.js

--- app/affilies/page.js
+++ app/affilies/page.js
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import BottomBar from '../components/BottomBar';
 import { AFFILIATE_LINK } from '../config/affiliate';
 
 export default function Affilies() {
