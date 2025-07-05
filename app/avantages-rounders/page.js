ef2128c5c01c897c021643421c0bf044ae75e510
Web-Online-Concept <web.online.concept@gmail.com>
2025-07-05 16:20:43 +0200

    Update page.js

--- app/avantages-rounders/page.js
+++ app/avantages-rounders/page.js
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import BottomBar from '../components/BottomBar';
 import { AFFILIATE_LINK, AFFILIATE_CODE } from '../config/affiliate';
 
 export default function AvantagesRounders() {
@@ -217,6 +218,7 @@ export default function AvantagesRounders() {
         </div>
       </div>
       <Footer />
+	  <BottomBar />
     </>
   );
 }
