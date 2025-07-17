f58e8cd7531bb16574832a8f0e8a7a203d43b8fe
Web-Online-Concept <web.online.concept@gmail.com>
2025-07-17 05:14:01 +0200

    Update layout.js

--- app/divers/layout.js
+++ app/divers/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'Articles & Infos diverses - Rounders Pro',
-  description: 'Découvrez nos articles et vidéos sur les paris sportifs, le casino et le poker ou en lien avec des jeux d'argent, le sport et autres',
+  description: 'Découvrez nos articles et vidéos sur les paris sportifs, le casino et le poker ou en lien avec des jeux d\'argent, le sport et autres',
 }
 
 export default function DiversLayout({ children }) {
